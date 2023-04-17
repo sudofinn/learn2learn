2da248a8b79741f6be3f5522eaf4c6e44fe70f80
sudofinn <gufi127127@gmail.com>
2023-04-17 14:42:38 +0200

    Also Fullwalkthrough not for ipad, must see

--- components/home/FullWalkthrough.tsx
+++ components/home/FullWalkthrough.tsx
@@ -6,7 +6,7 @@ import Script from 'next/script'
 
 const IntroVid = () => {
   return (
-     <div className='player-wrapper'>
+     <div className='player-wrapper flex md:hidden xl:flex'>
         <ReactPlayer
           className='react-player'
           url='https://www.youtube.com/watch?v=hJTwN6NKG-I'
